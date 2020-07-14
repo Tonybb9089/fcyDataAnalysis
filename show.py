a2dcfbb4270a1976263f2f794237957814a9e9ae
Tonybb9089 <18813033799@163.com>
2020-07-14 22:10:52 +0800

    add show.py to show the time of p1 and p2

---
+++ show.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', type=str, default="ls -al", help="the file name")
+args = parser.parse_args()
+file = args.f
+print("*******************************************************p1****************************")
+command_p1 = "cat "+file+" |strings|grep pre_commit_phase1|grep filcrypto"
+os.system(command_p1)
+iprint("******************************************************p2****************************")
+command_p2 = "cat "+file+" |strings|grep pre_commit_phase2|grep filcrypto"
+os.system(command_p2)
+
