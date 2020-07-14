4ab2e4ec9db4fccd9f076c63aca93334d2f086ad
Tonybb9089 <18813033799@163.com>
2020-07-14 18:42:26 +0800

    monk fix the x-axis bug

--- argumentparser.py
+++ argumentparser.py
@@ -1,9 +1,9 @@
 import argparse
 def ArgumentParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--readFile', type=str, default="", help="the location of the data file")
-    parser.add_argument('--writeFile', type=str, default="", help="the name of the generated figure")
-    parser.add_argument('--begin', type=str, default="", help="Jul-10 14:27:08")
-    parser.add_argument('--end', type=str, default="", help="Jul-10 14:27:08")
-    parser.add_argument('--item', type=str, default="", help="the name of column which you want to analysis")
+    parser.add_argument('--readFile', type=str, default="./a.csv", help="the location of the data file")
+    parser.add_argument('--writeFile', type=str, default="./demo.png", help="the name of the generated figure")
+    parser.add_argument('--begin', type=str, default="Jul-10 14:08:50", help="Jul-10 14:27:08")
+    parser.add_argument('--end', type=str, default="Jul-10 14:27:13", help="Jul-10 14:27:08")
+    parser.add_argument('--item', type=str, default="used", help="the name of column which you want to analysis")
     return parser.parse_args()
--- monk.py
+++ monk.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import argumentparser as argumentparser
+import matplotlib.dates as mdates
 args = argumentparser.ArgumentParser()
 
 from datetime import datetime
@@ -12,7 +13,6 @@ writeLocation = args.writeFile
 
 
 if __name__ == '__main__':
-
     df = pd.read_csv(readFilename)
     df['time'] = pd.to_datetime(df['time'],format='%b-%d %H:%M:%S')
     data = df[(df['time']>=pd.to_datetime(time_begin,format='%b-%d %H:%M:%S')) & (df['time']<=pd.to_datetime(time_end,format='%b-%d %H:%M:%S'))]
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     time_index = data.index
     data_time_translation = [str(datetime.strptime(str(d), '%Y-%m-%d %H:%M:%S').time()) for d in time_index]
     y_data = data[item].values
+    plt.title(item, size=20)
+    xticks = list(range(0, len(data_time_translation), int(len(data_time_translation)/10)))
+    plt.gcf().autofmt_xdate()
+    plt.xticks(xticks)
     plt.plot(data_time_translation, data[item],'b',lw=2.5)
-    plt.grid(True)
-    plt.axis("tight")
-    plt.title(item,size=20)
-    plt.savefig(writeLocation, bbox_inches='tight')
-
+    plt.savefig(writeLocation)
 
